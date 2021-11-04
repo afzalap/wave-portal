8ac86e5ddb2cc4fc92fc4f2ec1795c10f1f84ee2
Afzal Ahmed Pinjar <afzalap163@gmail.com>
2021-11-04 11:46:30 +0530

    Updates

---
+++ precache-manifest.265fe351e80bb18425eaf61e3054cdda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d81c96e66721dda2e6f0b875c5118d8",
+    "url": "/wave-portal/index.html"
+  },
+  {
+    "revision": "bbfa800ae70f23b162ab",
+    "url": "/wave-portal/static/css/main.34a712e3.chunk.css"
+  },
+  {
+    "revision": "d53470844d9d5d658abb",
+    "url": "/wave-portal/static/js/2.6fa69808.chunk.js"
+  },
+  {
+    "revision": "bbfa800ae70f23b162ab",
+    "url": "/wave-portal/static/js/main.d7b23549.chunk.js"
+  },
+  {
+    "revision": "9335e6e37c96e3f3dc95",
+    "url": "/wave-portal/static/js/runtime-main.51ee677a.js"
+  },
+  {
+    "revision": "77548118ec8e8b69128329004b02c94e",
+    "url": "/wave-portal/static/media/endless-constellation2.77548118.svg"
+  }
+]);
